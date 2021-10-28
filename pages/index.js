9deacd264a615de59669c6326ad885734d89b81e
tiararahmania05 <tiararahmania05@gmail.com>
2021-10-28 20:18:13 +0700

    Home Page Done

--- pages/index.js
+++ pages/index.js
@@ -17,14 +17,18 @@ export default function Home() {
         <Layout>
           <CarouselHome/>
           <CarouselSlide/>
-            <div class="grid grid-rows-1 md:grid-cols-5">
-              <div class="col-span-3 mr-8">
-                <ChildrenList/>
-              </div>
-              <div class="col-span-2">
-                <LiveEvent/>
-              </div>
-            </div>
+          <div className="md:mt-2">
+                <div className="container mx-auto max-w-full">
+                    <div className="grid grid-cols-1 xl:grid-cols-5">
+                        <div className="xl:col-start-1 xl:col-end-4 px-4 mb-14">
+                          <ChildrenList/>
+                        </div>
+                        <div className="xl:col-start-4 xl:col-end-6 px-4 mb-14">
+                          <LiveEvent/>
+                        </div>
+                    </div>
+                </div>
+          </div>
         </Layout>
       </div>
       
